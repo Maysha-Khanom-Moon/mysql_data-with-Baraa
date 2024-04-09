6f30c69f4e7f21efaf53c23fa703759e650e7922
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-04-09 16:49:24 +0600

    return all the rows from left table and only matching from right table

--- select.sql
+++ select.sql
@@ -1,7 +1,7 @@
 -- List customer ID, first name, order ID, quantity.
 -- Exclude the customers who have not placed any orders
 
--- inner join: return only the matching rows
+-- left join: return all recode from left table and only matching from the right
 
 SELECT 
 c.customer_id,
@@ -9,6 +9,6 @@ c.first_name,
 o.order_id,
 o.quantity
 FROM customers AS c
-INNER JOIN orders AS o
+LEFT JOIN orders AS o
 -- after ON: just put join key
 ON c.customer_id = o.customer_id
