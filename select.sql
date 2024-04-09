ee5eae1069c24d384a8078339477321180498856
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-04-10 03:09:35 +0600

    detact more appropiately the space and length difference

--- select.sql
+++ select.sql
@@ -3,6 +3,8 @@
 -- LENGTH(): length of string
 
 SELECT
+last_name,
 TRIM(last_name) AS clean_last_name,
-LENGTH(last_name) AS len_last_name
+LENGTH(last_name) AS len_last_name,
+LENGTH(TRIM(last_name)) AS clean_len_last_name
 FROM customers
