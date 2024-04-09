c367ab6e28873cf5820c9e1bb26cfdb42aa3dbde
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-04-09 14:35:03 +0600

    logican or operator to join multiple comparison for where

--- select.sql
+++ select.sql
@@ -1,8 +1,8 @@
--- Find all customers who come from Germany and whose score less than 400
+-- Find all customers who come from Germany or whose score less than 400
 
 -- WHERE
 -- Logical operators: AND, OR, NOT
 
 SELECT * 
 FROM customers
-WHERE country = 'Germany' AND score < 400
+WHERE country = 'Germany' OR score < 400
