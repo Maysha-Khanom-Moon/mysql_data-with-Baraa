597e06694292202fced360c2bece08dac0bed38f
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-04-10 02:16:43 +0600

    count the total rows of any table

--- select.sql
+++ select.sql
@@ -1,26 +1,9 @@
--- List first name, last name and country of all persons from customers and employees
+-- Find the total number of customers
 
--- UNION ALL: cobines the rows
--- there is no check for duplicate values
+-- COUNT(): total number of rows
 
--- UNION: cobines the rows and it removes any duplicate
--- make sure both table have same number of columns and order of column
+-- AS used for renaming
+-- but it didn't change the original table
 
--- make sure both table have same number of columns and order of columns
--- because UNION does not check the order of columns. It just combined rows
-
-SELECT
-	first_name AS per_first_name,
-    last_name AS per_last_name,
-    country AS per_country
+SELECT COUNT(*) AS total_customers
 FROM customers
-
--- UNION ALL
-UNION
-
-SELECT
-	last_name,
-    first_name,
-    emp_country
-    -- salary
-FROM employees
