448c3125f812f15f80e5a231866328cde69d6da0
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-04-09 16:42:49 +0600

    INNER JOIN and aliases and ON for join key

--- select.sql
+++ select.sql
@@ -1,5 +1,14 @@
--- SQL Aliases -- AS
+-- List customer ID, first name, order ID, quantity.
+-- Exclude the customers who have not placed any orders
+
+-- inner join: return only the matching rows
 
 SELECT 
-	c.customer_id
+c.customer_id,
+c.first_name,
+o.order_id,
+o.quantity
 FROM customers AS c
+INNER JOIN orders AS o
+-- after ON: just put join key
+ON c.customer_id = o.customer_id
