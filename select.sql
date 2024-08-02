6892c13af48147d14594f098c8b395ea5d2b5577
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-08-02 23:48:11 +0600

    GROUP BY just done by aggregate functions

--- select.sql
+++ select.sql
@@ -1,7 +1,17 @@
 -- Find the total number of customers for each country
 
+-- SELECT 
+-- 	country,
+--  COUNT(*) as total_customers
+-- FROM customers
+-- GROUP BY country 
+-- ORDER BY COUNT(*) ASC
+
+
+-- Find the highest score of customers for each country
 SELECT 
 	country,
-    COUNT(*) as total_customers,
+    MAX(score) as highest_score
 FROM customers
-GROUP BY country 
+GROUP BY country
+ORDER BY MAX(score) ASC
